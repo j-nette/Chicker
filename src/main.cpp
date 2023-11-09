129efb3e73977f8d71e9a4183f8754f5c0d91cf5
Jeanette <hiddensquiddy@gmail.com>
2023-11-09 11:05:04 -0800

    simple skeleton with serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,65 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define KICK 1
+#define CHIP 2
+#define DRIBBLE 3
+
+int checkAction(void);
+
+void kick(void);
+void chip(void);
+void dribble(void);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+
+  Serial.println("1 for kick - 2 for chip - 3 for dribble");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int action = checkAction();
+
+  switch(action){
+  case 1:
+    kick();
+    break;
+  case 2:
+    chip();
+    break;
+  case 3:
+    dribble();
+    break;
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+int checkAction() { 
+  int action = 0;
+  int waiting = 1;
+  do{
+    while(Serial.available() == 0){} //waiting for user input
+    action = Serial.parseInt(); // reading user input from serial monitor
+    if(action == CHIP || action == KICK || action == DRIBBLE){
+      waiting = 0;
+    }
+    else{
+      Serial.println("Error: not a valid input. 1 for kick - 2 for chip - 3 for dribble");
+    }
+  }while(waiting);
+
+  return action;
+}
+
+void kick(){
+  /*do something*/
+  Serial.println("I kicked :)");
+}
+
+void chip(){
+  /*do something*/
+  Serial.println("I chipped :)");
+}
+
+void dribble(){
+  /*do something*/
+  Serial.println("I dribbled :)");
 }
