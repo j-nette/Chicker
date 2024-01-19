b9f9cb84f85e3b55ee6ed313ed2fd7f2d0823e59
Jeanette <hiddensquiddy@gmail.com>
2024-01-18 22:08:36 -0800

    updated gui with action log

--- playground/gui.py
+++ playground/gui.py
@@ -1,39 +1,39 @@
 import tkinter as tk
-import serial
-import time
-
-arduinoData = serial.Serial(port='COM6', baudrate=9600, timeout=.1) 
-
-def write_read(x: str): 
-	arduinoData.write(bytes(x, 'utf-8')) 
-	time.sleep(0.05) 
-	#data = arduinoData.readline() 
-	#return data 
 
 def sendKick():
-	write_read("1," + str(slider.get()))
+	#do something
+
+	Text.insert('1.0',"Kick with " + str(slider.get()) + "!\n")
 
 def sendChip():
-	write_read("2," + str(slider.get()))
+	#do something
+	Text.insert('1.0',"Chip with " + str(slider.get()) + "!\n")
 
 def sendCharge():
-	write_read("3," + str(slider.get()))
-
+	#do something
+	Text.insert('1.0',"Charge with " + str(slider.get()) + "!\n")
 
 # tkinter window setup
+
 chicker_tester_window = tk.Tk()
+chicker_tester_window.geometry("500x300")
+
+Text = tk.Text(chicker_tester_window, width = 40, height = 4, wrap = "word")
+Text.insert('1.0',"Hi c:")
 
 Button = tk.Button
 
-kickBtn = Button(chicker_tester_window, text = "Kick", command = sendKick)
-chipBtn = Button(chicker_tester_window, text = "Chip", command = sendChip)
-chargeBtn = Button(chicker_tester_window, text = "Charge", command = sendCharge)
+kickBtn = Button(chicker_tester_window, text = " Kick ", font = ("helvetica, 16"), command = sendKick)
+chipBtn = Button(chicker_tester_window, text = " Chip ", font = ("helvetica, 16"), command = sendChip)
+chargeBtn = Button(chicker_tester_window, text = "Charge", font= ("helvetica, 16"),command = sendCharge)
 
 slider = tk.Scale(chicker_tester_window, from_=0, to=200, orient="horizontal")
 
-kickBtn.grid(row = 0, column = 1)
-chipBtn.grid(row = 0, column = 2)
-chargeBtn.grid(row = 0, column = 3)
-slider.grid(row = 1)
+
+kickBtn.place(relx=0.30, rely=0.35, anchor="center")
+chipBtn.place(relx=0.5, rely=0.35, anchor="center")
+chargeBtn.place(relx=0.70, rely=0.35, anchor="center")
+slider.place(relx=0.5, rely=0.65, anchor = "center")
+Text.place(relx=0.5, rely=0.14, anchor = "center")
 
 chicker_tester_window.mainloop()
